364acec08a78cdfdb25ddb1e4decc64a99aeba11
roeecohen5@gmail.com <Roee1212>
2020-03-17 17:32:45 +0200

    nnn

--- app/src/main/java/com/example/coockit/HomeFragment.java
+++ app/src/main/java/com/example/coockit/HomeFragment.java
@@ -4,16 +4,91 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.squareup.picasso.Picasso;
 
 public class HomeFragment extends Fragment {
+    private DatabaseReference databaseRef;
+    private RecyclerView recyclerView;
+    private FirebaseRecyclerAdapter<Recipe, HomeFragment.HomeRecipesViewHolder> adapter;
+
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view =  inflater.inflate(R.layout.fragment_home, container, false);
+
+        recyclerView = view.findViewById(R.id.home_recycle_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        databaseRef = FirebaseDatabase.getInstance().getReference("Recipes");
+        databaseRef.keepSynced(true);
+        FirebaseRecyclerOptions<Recipe> options = new FirebaseRecyclerOptions.Builder<Recipe>()
+                .setQuery(databaseRef, Recipe.class).build();
+
+        adapter = new FirebaseRecyclerAdapter<Recipe, HomeRecipesViewHolder>(options) {
+
+            @NonNull
+            @Override
+            public HomeRecipesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.home_recipe_item,parent,false);
+
+                // here we override the inflated view's height to be half the recyclerview size
+                ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) view.getLayoutParams();
+                layoutParams.height = (parent.getHeight() / 3) - layoutParams.topMargin - layoutParams.bottomMargin;
+                view.setLayoutParams(layoutParams);
+
+                return new HomeFragment.HomeRecipesViewHolder(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull HomeRecipesViewHolder holder, int position, @NonNull Recipe model) {
+                holder.recipeName.setText(model.getName());
+                Picasso.get().load(model.getPicUrl()).into(holder.imgView);
+
+            }
+        };
+        recyclerView.setAdapter(adapter);
+
+        return view;
     }
 
+    private class HomeRecipesViewHolder extends RecyclerView.ViewHolder {
+        private TextView recipeName;
+        private ImageView imgView;
+
+        public HomeRecipesViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            recipeName = (TextView)itemView.findViewById(R.id.home_rec_name);
+            imgView = (ImageView)itemView.findViewById(R.id.home_rec_img);
+        }
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        adapter.stopListening();
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        adapter.startListening();
+    }
 }
