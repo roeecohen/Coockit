bf290ea4a1de299c7005b0ce7899989fece9ee94
roeecohen5@gmail.com <Roee1212>
2020-03-19 13:53:59 +0200

    home change

---
+++ app/src/main/java/com/example/coockit/RecipeViewHolder.java
@@ -0,0 +1,52 @@
+package com.example.coockit;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecipeViewHolder extends RecyclerView.ViewHolder {
+    private TextView recipeName;
+    private TextView ingerdients;
+    private TextView directions;
+    private TextView difficulty;
+    private TextView prep;
+    private ImageView imgView;
+
+    public TextView getRecipeName() {
+        return recipeName;
+    }
+
+    public TextView getIngerdients() {
+        return ingerdients;
+    }
+
+    public TextView getDirections() {
+        return directions;
+    }
+
+    public TextView getDifficulty() {
+        return difficulty;
+    }
+
+    public TextView getPrep() {
+        return prep;
+    }
+
+    public ImageView getImgView() {
+        return imgView;
+    }
+
+    public RecipeViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        recipeName = (TextView)itemView.findViewById(R.id.rec_name);
+        ingerdients = (TextView)itemView.findViewById(R.id.rec_ingred);
+        directions = (TextView)itemView.findViewById(R.id.rec_dire);
+        difficulty = (TextView)itemView.findViewById(R.id.rec_diffi);
+        prep = (TextView)itemView.findViewById(R.id.rec_prep);
+        imgView = (ImageView)itemView.findViewById(R.id.rec_img);
+    }
+}
