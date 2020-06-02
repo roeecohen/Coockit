ae5a235137b63a90d43cec6ce794b0a52ca20929
roeecohen5@gmail.com <Roee1212>
2020-06-02 16:26:07 +0300

    home page update3

--- app/src/main/java/com/example/coockit/Search/SearchFragment.java
+++ app/src/main/java/com/example/coockit/Search/SearchFragment.java
@@ -1,5 +1,7 @@
 package com.example.coockit.Search;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -63,6 +65,7 @@ public class SearchFragment extends Fragment {
         mCheckAroundBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
 
             }
         });
